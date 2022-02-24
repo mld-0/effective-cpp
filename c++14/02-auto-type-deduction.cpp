52a169d1a58a33f47ce7a0e7d15b7d5fd8e45e72
Matthew L Davis <mld.0@protonmail.com>
2022-02-24 19:42:34 +1100

    cleanup
    TODO is clarify rules for 'auto&& uref3 = 27;'

--- c++14/02-auto-type-deduction.cpp
+++ c++14/02-auto-type-deduction.cpp
@@ -1,8 +1,17 @@
+//	VIM SETTINGS: {{{3
+//	vim: set tabstop=4 modeline modelines=10 foldmethod=marker:
+//	vim: set foldlevel=2 foldcolumn=2:
+//	}}}1
 #include <initializer_list>
 #include <iostream>
 #include <vector>
 #include <string>
 using namespace std;
+//	{{{2
+
+//	For Vim with YCM, check deduced types with
+//			:YcmCompleter GetType
+
 
 //	Given function template:
 //			template<typename T>
@@ -35,14 +44,13 @@ int main()
 {
 	//	As for deducing template types, there are three cases:
 	//	1) The type specifier is a pointer or a reference (but not a forwarding reference)
-	//	If expr's type is a reference, ignore the reference part. Then pattern match expr's type against ParamType to determine T
+	//	Rule: If expr's type is a reference, ignore the reference part. Then pattern match expr's type against ParamType to determine T
 
 	//	2) The type specifier is a forwarding reference
-	//	If expr is an lvalue, both T and ParamType are deduced to be lvalue references (this is the only situtation where T is deduced to be a reference).
-	//	If expr is an rvalue: If expr's type is a reference, ignore the reference part. Then pattern match expr's type against ParamType to determine T (that is, Case 1 rules apply)
+	//	Rule: If expr is an lvalue, both T and ParamType are deduced to be lvalue references (this is the only situtation where T is deduced to be a reference). If expr is an rvalue: If expr's type is a reference, ignore the reference part. Then pattern match expr's type against ParamType to determine T (that is, Case 1 rules apply)
 
 	//	3) The type specifier is neither a pointer nor a reference (pass-by-value)
-	//	If expr's type is a reference, ignore the reference part. If expr is const or volatile, ignore that too.
+	//	Rule: If expr's type is a reference, ignore the reference part. If expr is const or volatile, ignore that too.
 
 	auto x = 27;				//	T = int
 	const auto cx = x;			//	T = int, ParamType = const int
@@ -50,8 +58,9 @@ int main()
 
 	auto&& uref1 = x;			//	x is lvalue, T = int&
 	auto&& uref2 = cx;			//	cx is lvalue, T = const int&
-	auto&& uref3 = 27;			//	27 is rvalue, T = int&&	
-	//	Ongoing: 2022-02-20T17:52:26AEDT non-const-ness of a 'constant' value? (uref3 can be changed (I checked))
+
+	auto&& uref3 = 27;			//	27 is rvalue, T = int&&		<(Being what the book says, but it isn't, YCM says T = int, and I can take its address?)>
+	//	TODO: 2022-02-21T02:43:50AEDT effective-c++, item 02, auto-type-deduction, 'auto&& uref3 = 27' is T = int or T = int&& (and how to check?) -> (I can take its address (and I can change it) -> must be 'int'?) (where are the rules on this and what do they say?) (and if T = int, declare a 'uref4' for which T = int&&) (this is all to do with universal references and nothing to do with auto -> 'int&& uref3 = 27' <appears> to yield same result? meaning this is a topic for) (see item 24)
 
 	//	Array and Function names decay into pointers for non-reference type specifiers
 	const char name[] = "R. N. Briggs";
@@ -59,7 +68,7 @@ int main()
 	auto& arr2 = name;			//	T = const char (&)[13]
 	//	void someFunc(int,double)
 	auto func1 = someFunc;		//	T = void (*)(int,double)
-	auto func2 = someFunc;		//	T = void (&)(int,double)
+	auto& func2 = someFunc;		//	T = void (&)(int,double)		<(YCM: void(int,double))>
 
 
 	//	Example: when the initalizer for an auto variable is enclosed in braces {}, the deduced type is initalizer_list
@@ -72,9 +81,9 @@ int main()
 
 
 	//	Example: braces {} and template type deduction
-	//	void f_i(T)
+	//		void f_i(T)
 	//f_i( { 11,23,9 } );		//	error, cannot deduce T
-	//	void f_ii(intializer_list<T>)
+	//		void f_ii(intializer_list<T>)
 	f_ii( {11,23,9} );			//	T = int, ParamType = std::initalizer_list<int>
 
 
