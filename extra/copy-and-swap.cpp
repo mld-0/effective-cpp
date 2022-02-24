1deca27fa7b00cea1fcae0ac61c1ca3818acf2db
Matthew L Davis <mld.0@protonmail.com>
2022-02-24 21:03:20 +1100

    ongoing

--- extra/copy-and-swap.cpp
+++ extra/copy-and-swap.cpp
@@ -8,12 +8,12 @@ using namespace std;
 //	LINK: https://stackoverflow.com/questions/3279543/what-is-the-copy-and-swap-idiom
 //	LINK: https://stackoverflow.com/questions/5695548/public-friend-swap-member-function
 
-//	copy-and-swap is a technique for implementing copy-assignment (and move-assignment), in terms of the copy-constructor, and with a strong no exception guarantee. It involves making a temporary copy of the object with the copy-constructor (or move-constructor), then swapping the contents of the current object with said temporary object. It solves the problem of self-assignment without requiring a test for self-assignment
-//	It is a useful solution when the rule-of-zero cannot be followed. The extra memory required for the temporary object is the tradeoff required for exception safety
-//	copy-and-swap requires that we provide our own swap function for the class in question, (std::swap uses copy-assignment and therefore cannot be used to implement copy-assignment) (it also requires a copy-constructor and a destructor for that class)
+//	copy-and-swap is a technique for implementing copy-assignment (and move-assignment), in terms of the copy-constructor, and with a strong no exception guarantee. It involves making a temporary copy of the object with the copy-constructor (or move-constructor), then swapping the contents of the current object with said temporary object. It solves the problem of self-assignment without requiring a test for self-assignment.
+//	It is a useful solution when the rule-of-zero cannot be followed. The extra memory required for the temporary object is the tradeoff required for exception safety.
+//	copy-and-swap requires that we provide our own swap function for the class in question, (std::swap uses copy-assignment and therefore cannot be used to implement copy-assignment) (it also requires a copy-constructor and a destructor for that class).
 //	'swap()' is a non-throwing function that swaps the members of two classes. 
 
-//	TODO: 2022-01-25T22:37:54AEDT effective-c++, extra/copy-and-swap, what (special member functions) can/should be marked as noexcept? (making things explict) (vs the compiler being able to deduce noexcept for straightforward functions?)
+//	TODO: 2022-02-11T00:53:44AEDT effective-c++, extra/copy-and-swap, noexcept, what (special member functions) can/should be marked as noexcept? (making things explict) (vs the compiler being able to deduce noexcept for straightforward functions?)
 
 class dumb_array
 {
@@ -32,7 +32,7 @@ public:
           mArray(mSize ? new int[mSize] : nullptr)
     {
 		cout << "dumb_array copy-constructor\n";
-        // note that this is non-throwing, because of the data types being used; more attention to detail with regards to exceptions must be given in a more general case, however
+        // note that this is non-throwing because the data types being used <(are non-throwing when copied?)>; more attention to detail with regards to exceptions must be given in a more general case, however
         std::copy(other.mArray, other.mArray + mSize, mArray);
     }
 
@@ -52,7 +52,7 @@ public:
 
 	//	because <reasons> <the alternative is problematic> (requires two functions, one to be found via ADL and the other to handle std:: qualifications) ((something to do with) partial template specialization)
 	//	swap is best implemented as a <member> friend function
-	//	a friend function defined inside a class is: 1) placed in the enclosing namespace, 2) automatically inline, 3) able to refer to static members of the class without further qualification
+	//	a friend function defined inside a class is: 1) placed in the enclosing namespace, 2) automatically inline, and 3) able to refer to static members of the class without further qualification
 	friend void swap(dumb_array& lhs, dumb_array& rhs) 	//	noexcept?
 	{
 		cout << "dumb_array (friend) swap\n";
@@ -60,10 +60,11 @@ public:
 		using std::swap;
 		swap(lhs.mSize, rhs.mSize);
 		swap(lhs.mArray, rhs.mArray);
+		//	any new member variables will also need to be swaped
 	}
 
 	//	copy/move-assignment
-	dumb_array& operator=(dumb_array rhs) //	note pass by value: <copy-constructs for lvalue arguments, move-constructs for rvalue arguments?>
+	dumb_array& operator=(dumb_array rhs) //	note: pass by value <copy-constructs for lvalue arguments, move-constructs for rvalue arguments?>
 		//	noexcept?
 	{
 		cout << "dumb_array assignment\n";
@@ -102,7 +103,7 @@ int main()
 	for (auto i = 0; i < B.size(); ++i) cout << B[i] << " ";
 	cout << "\n\n";
 
-	cout << "initalize C and copy-assign from B\n";
+	cout << "default initalize C and copy-assign from B\n";
 	dumb_array C;
 	C = B;
 	for (auto i = 0; i < C.size(); ++i) cout << C[i] << " ";
