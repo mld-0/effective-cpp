e5818f30b7079b90084cc64b2297a2aa2cae72a9
Matthew L Davis <mld.0@protonmail.com>
2022-03-02 02:26:41 +1100

    ongoing
    Consider, 'move-vs-forward' is a better name, but git hates renames?
    TODO is finish outlining implicit move ctor/assignment rules.

--- c++14/23-move-forward.cpp
+++ c++14/23-move-forward.cpp
@@ -8,6 +8,7 @@
 using namespace std;
 //	{{{2
 class Widget;
+//	Ongoing: 2022-03-02T02:24:26AEDT document should be 'move-vs-forward' (<why> is git so bad with renaming files?)
 
 //	On l/r-value-ness:
 //	{{{
@@ -28,6 +29,26 @@ class Widget;
 
 //	Move Semantics: makes it possible to replace expensive copies with cheaper moves. The move-ctor and move-assignment member functions control the semantics of moving.  <(<Default/implicit> class support for move?)> <(Move only types (are those with deleted copy ctor/assignment?))>
 //	Perfect Forwarding: makes it possible to write function templates that take arbitrary arguments and forward them to a third function as they were recieved.
+//	Unless a type supports move assignment/construction, a move is just as copy.
+
+//	TODO: 2022-03-02T02:24:05AEDT effective-c++, item 23, move-forward, review implicit move assignment/ctor rules (having copied the ctor rules to be cleaned up, along with a statement of presumption that implicit move assignment works the same way?)
+//	Default move assignment/constructor behaviour:
+//		'= default' move ctor/assign performs a move on each class member variable
+//		The implicit move ctor</assign?> of a class is created if: <(1: the class does not have any other special member functions declared <anything, like any ctor, any copy, ect?>, and 2: the move ctor can be sensibly implemented by moving all its members and bases (what is 'members and bases'), (how is the compiler assessing this? is it a question of is move supported for all member variables?))>
+//	Rule (cppreference):
+//	{{{
+//	LINK: https://en.cppreference.com/w/cpp/language/move_constructor
+//	If no user-defined move constructors are provided for a class type (struct, class, or union), and all of the following is true:
+//	there are no user-declared copy constructors;
+//	there are no user-declared copy assignment operators;
+//	there are no user-declared move assignment operators;
+//	there is no user-declared destructor.
+//	then the compiler will declare a move constructor as a non-explicit inline public member of its class with the signature T::T(T&&).
+//	A class can have multiple move constructors, e.g. both T::T(const T&&) and T::T(T&&). If some user-defined move constructors are present, the user may still force the generation of the implicitly declared move constructor with the keyword default.
+//	The implicitly-declared (or defaulted on its first declaration) move constructor has an exception specification as described in dynamic exception specification (until C++17)noexcept specification (since C++17)
+//	}}}
+//	Ongoing: 2022-03-02T02:22:12AEDT can one presume the implicit move-assignment operator follows same/similar rules?
+
 
 //	rvalue references are the language feature that makes both features possible. 
 //	The construct 'T&&' does not always represent an rvalue reference.
@@ -36,12 +57,10 @@ class Widget;
 void f(Widget&& w_arg); 
 //	'w_arg' is an lvalue <(what is the (meaning of the) address of 'w_arg'?)>
 
-//	Ongoing: 2022-02-26T01:44:18AEDT function-template vs template-function
 //	'std::move()' and 'std::forward()' are function templates that perform casts.
 
 //	Example: approximation of 'std::move()', casts 'param' to an rvalue (for reference collapsing see item 28)
-//	<(Does not actually move anything, performs a cast)>
-//	The result of applying move to an object is that <the result> is an rvalue
+//	The result of applying move to an object is that it is cast to an rvalue
 template<typename T>
 decltype(auto) my_move(T&& param) {	
 	using RT = typename remove_reference<T>::type&&;
