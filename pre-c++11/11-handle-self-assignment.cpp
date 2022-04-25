1e485167378aef5670216b21eec4b7f7981dc5b9
Matthew L Davis <mld.0@protonmail.com>
2022-04-26 00:11:38 +1000

    TODO is use best-practice 'swap()'

--- pre-c++11/11-handle-self-assignment.cpp
+++ pre-c++11/11-handle-self-assignment.cpp
@@ -35,24 +35,24 @@ int main()
 	//	Ongoing: 2022-01-25T06:58:16AEDT there is (a lot) more to be said about (the different kinds of) 'swap()' function ... how's about (some kind of) default?
 	//	TODO: 2022-01-25T07:03:21AEDT effective-c++, 11-handle-self-assignment, (this being a slightly complicated topic of which <from the book we adapt a half finished example (further)>), how best to do the 'swap()' in 'copy-and-swap' is a somewhat involved subject (swap as member/friend function, implemented best / in terms of a default?) (does copy-and-swap warrent a dedicated example (something the book doesn't make a dedicated item?)) (best copy and swap implements both move and swap (together)?)
 
-	//	the better solution is exception safe assignment ((in this case) exception safety is also self assignment safety) (for exception safety see item 29)
-	//	copy-and-swap exception safe solution
-	class Widget {
-		void swap(Widget& rhs) {		//	exchange data of 'this' and 'rhs', (see item 29 on exception safety)
-		}
-		//friend void swap(Widget& lhs, Widget& rhs) {
-		//	using std::swap;
-		//	//swap(
-		//}
-		Widget& operator=(const Widget& rhs) {
-			Widget temp(rhs);
-			//swap(*this, temp);
-			swap(temp);
-			return *this;
-		}
-	};
-
-	Widget x;
+	//	TODO: 2022-01-26T21:17:17AEDT Better copy-and-swap example
+	////	the better solution is exception safe assignment ((in this case) exception safety is also self assignment safety) (for exception safety see item 29)
+	////	copy-and-swap exception safe solution
+	//class Widget {
+	//	void swap(Widget& rhs) {		//	exchange data of 'this' and 'rhs', (see item 29 on exception safety)
+	//	}
+	//	//friend void swap(Widget& lhs, Widget& rhs) {
+	//	//	using std::swap;
+	//	//	//swap(
+	//	//}
+	//	Widget& operator=(const Widget& rhs) {
+	//		Widget temp(rhs);
+	//		//swap(*this, temp);
+	//		swap(temp);
+	//		return *this;
+	//	}
+	//};
+	//Widget x;
 
 	return 0;
 }
