aad0064bdace5e4b47d15ab8afd9defd20d862e5
Matthew L Davis <mld.0@protonmail.com>
2022-05-04 04:04:04 +1000

    ongoing
    cleanup pending / dump on topic
    Form/parts of a lambda. Several incomplete definitions.
    Many ongoings: continue through links, (can one expect problems) calling
    non-const member functions of captured objects(?), and use of (and
    copy-pasting) terms without providing definitions.

--- extra/lambdas-and-alternatives.cpp
+++ extra/lambdas-and-alternatives.cpp
@@ -68,8 +68,124 @@ constexpr bool is_lvalue(T&& x) {
 //	Ongoing: 2022-05-04T02:37:15AEST meaning/example of/for closure class?
 
 
-//	Lambda syntax:
+//	LINK: https://en.cppreference.com/w/cpp/language/lambda
+//	{{{
+//	Forms of a lambda:
+//	1		[ captures ] ( params ) <(specs)> <(requires(optional))> { body }
+//	2		[ captures ] { body }																			<C++23>
+//	2		[ captures ] <(specs)> { body }																	<C++23>
+//	3		[ captures ] <tparams> <(requires)> (params) <(specs)> <(requires(optional))>  { body }			<C++20>
+//	4		[ captures ] <tparams> <(requires(optional))> { body }							<since-C++20, until-C++23>
+//	4		[ captures ] <tparams> <(requires(optional))> <(specs)> { body }								<C++23>
+//		1	Full form
+//		2	Omitted parameters list
+//		3	Same as 1, but specifies a generic lambda and explicitly proves a list of template parameters
+//		4	Same as 2, but specifies a generic lambda and explicitly proves a list of template parameters
+//	captures: comma separated list of zero/more captures, (optionally) beginning with <(capture-default?)>
+//				can use variable without capturing it if: 
+//						it is non-local, static, or thread local storage duration *1
+//						it is a reference that has been initialized with a constant-expression
+//				can read value without capturing it if:
+//						has const non-volatile integral/enumeration type, and has been initialized by constant-expression
+//						is constexpr and has no mutable members
+//				*1	(in which case the variable cannot be captured)
+//	tparams: non-empty comma-separated list of template parameters 
+//				<(provide names to the template parameters of a generic lambda, see 'ClosureType::operator()')>
+//	params: list of parameters (as in named function)
+//	specs: <(specifiers, exception, attr, and trailing-return-type (in order) (each being optional))>
+//	specifiers: if not provided, objects capture by copy are const in lambda body
+//				mutable, allows body to modify objects called by copy and to call their non-const member functions
+//				constexpr, function-call-operator/operator-template-specialization is a constexpr function
+//				consteval, function-call-operator/operator-template-specialization is an immediate function
+//				(contexpr/consteval are mutually exclusive)
+//	exception: provide dynamic-exception-specification, (or <pre-C++20> the 'noexcept' specifier
+//	attr: attribute-specification(?) <(for the type of the function-call-operator/operator-template of the closure type (any attribute specified does not appertain to the function call operator or operator template itself, but its type?))>
+//	trailing-return-type: <(ret, where ret specifies the return type. If trailing-return-type is not present, the return type of the closure's operator() is deduced from return statements as if for a function whose return type is declared auto)>
+//	requires: <since-C++20> add constraints(?) to the 'operator()' of the closure type
+//	body: function body
+
+//	<(If 'auto' is used as a type of a parameter (or <since-C++20> an-explicit-template-parameter-list(?) is provided), the lambda is a generic-lambda)>
+
+//	<(Dangling references: if a non-reference entity is captured by reference (explicitly or implicitly), and the function call operator or a specialization of the function call operator template of the closure object is invoked after the entity's lifetime has ended, undefined behavior occurs. The C++ closures do not extend the lifetimes of objects captured by reference (Same applies to the lifetime of the current *this object captured via this))>
+
+//	Lambda-capture: 
+//	{{{
+//		<>
+//	1) simple by-copy capture
+//	2) simple by-copy capture that is a pack expansion
+//	3) by-copy capture with an initializer
+//	4) simple by-reference capture
+//	5) simple by-reference capture that is a pack expansion
+//	6) by-reference capture with an initializer
+//	7) simple by-reference capture of the current object
+//	8) simple by-copy capture of the current object
+//	9) by-copy capture with an initializer that is a pack expansion
+//	10) by-reference capture with an initializer that is a pack expansion
+//	}}}
+
+
+//	Ongoing: 2022-05-04T03:49:44AEST 'cppreference' provides extensive/exhorbitant/unhelpfully-detailed details on the lambda -> better results from (starting with) friendlier <reference/source>-material?
+//	Ongoing: 2022-05-04T03:37:54AEST lambdas, (problems?) calling non-const member functions of captured objects(?)
+//	Ongoing: 2022-05-04T03:40:53AEST (a lot of) words above used above (without providing the <C++> definition) (i.e: attribute-specification, operator-template-specialization, dynamic-exception-specification, <...>) 
+//	Ongoing: 2022-05-04T03:45:35AEST later definitions (see above) copied, (their) <meaning/definition> still unclear
+//	}}}
+
+
+//	LINK: https://docs.microsoft.com/en-us/cpp/cpp/lambda-expressions-in-cpp?view=msvc-170
+//	{{{
+//	Parts of a Lambda:
+//	[=] () mutable throw() -> int { ... }
+//		1)	'[=]' 			capture-clause (or lambda-introducer)
+//		2)	'()' 			parameter-list (optional) (or lambda-declarator)
+//		3)	'mutable' 		mutable-specification (optional)
+//		4)	'throw()' 		exception-specialization
+//		5)	'-> int'		trailing-return-type (optional)
+//		6)	{ ... }			lambda-body
+
+//	capture clause:
 //	<>
+
+//	generalized capture <C++14>:
+//	<>
+
+//	parameter-list: 
+//	<>
+
+//	mutable-specification:
+//	<>
+
+//	exception-specification:
+//	<>
+
+//	return-type:
+//	<>
+
+//	lambda-body:
+//	<>
+
+//	constexpr lambda expressions:
+//	<>
+
+//	<(Microsoft specific: lambdas are not supported by 'ref class', 'ref struct', 'value class', or 'value struct')>
+//	}}}
+
+
+//	Ongoing: 2022-05-04T04:00:59AEST continue through links until a complete picture of lambdas appears?
+//	LINK: https://stackoverflow.com/questions/7627098/what-is-a-lambda-expression-in-c11
+//	{{{
+//	}}}
+//	LINK: https://www.geeksforgeeks.org/lambda-expression-in-c/
+//	{{{
+//	}}}
+//	LINK: https://towardsdatascience.com/c-basics-understanding-lambda-7df00705fa48
+//	{{{
+//	}}}
+//	LINK: https://www.cprogramming.com/c++11/c++11-lambda-closures.html
+//	{{{
+//	}}}
+//	LINK: https://www.softwaretestinghelp.com/lambdas-in-cpp/
+//	{{{
+//	}}}
 
 
 //	Capture modes: <C++11> by-reference and by-value
