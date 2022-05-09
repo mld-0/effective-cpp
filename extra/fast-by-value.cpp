93fe755cda5637b4b45045a718e1db5cf9529a82
Matthew L Davis <mld.0@protonmail.com>
2022-05-10 01:51:29 +1000

    extra/fast-by-value
    links / to be continued
    Fast passing of objects by-value: copy-elision/RVO/(?)

---
+++ extra/fast-by-value.cpp
@@ -0,0 +1,49 @@
+//	VIM SETTINGS: {{{3
+//	vim: set tabstop=4 modeline modelines=10 foldmethod=marker:
+//	vim: set foldlevel=2 foldcolumn=2:
+//	}}}1
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+//	{{{2
+
+
+//	<C++?> idiom: 'Want speed? Pass by value'
+//	LINK: https://web.archive.org/web/20140205194657/http://cpp-next.com/archive/2009/08/want-speed-pass-by-value/
+//	LINK: https://stackoverflow.com/questions/12953127/what-are-copy-elision-and-return-value-optimization
+
+//	copy-elision: omit copy/move ctors, and their side effects, resulting in zero-copy pass-by-value semantics
+//	LINK: https://en.cppreference.com/w/cpp/language/copy_elision
+
+//	Fastest, call by value/reference/poitner: (only way to tell is to profile code)
+
+//	epsilon: <(minimum floating point error? other?)>
+
+
+//	return-value-optimisation:
+
+
+
+
+//	LINK: https://stackoverflow.com/questions/35506708/move-constructor-vs-copy-elision-which-one-gets-called
+//	{{{
+//	}}}
+
+//	LINK: https://stackoverflow.com/questions/12953127/what-are-copy-elision-and-return-value-optimization
+//	{{{
+//	}}}
+
+//	LINK: https://en.cppreference.com/w/cpp/language/copy_elision
+//	{{{
+//	}}}
+
+//	LINK: https://news.ycombinator.com/item?id=1442756
+//	{{{
+//	}}}
+
+int main()
+{
+	return 0;
+}
+
